e2f6f7c56964a298b9fede0067f69c9eaf3b67d5
senaya <vivisectivecarrot@gmail.com>
2020-06-10 15:12:36 +0200

    hw1

---
+++ hw1/task5/index.php
@@ -0,0 +1,37 @@
+﻿<?php
+	$a = 1;
+	$b = 2;
+
+echo "a = {$a}, b = {$b}\n";
+
+// the fastest way
+function var1($a, $b) {
+	$temp = $a;
+	$a = $b;
+	$b = $temp;
+	echo "a = {$a}, b = {$b}\n";
+};
+
+var1($a, $b);
+
+// only works with numbers
+function var2($a, $b) {
+	$a ^= $b ^= $a ^= $b;
+	echo "a = {$a}, b = {$b}\n";
+};
+
+var2($a, $b);
+
+// slower because of the function call
+function var3($a, $b) {
+	list($a,$b) = array($b,$a);
+	echo "a = {$a}, b = {$b}\n";
+};
+
+var3($a, $b);
+
+
+
+echo "a = {$a}, b = {$b}\n";
+
+?>

39276e0618fa68011bb95135b64ef5550c24adce
senaya <vivisectivecarrot@gmail.com>
2020-06-10 14:38:32 +0200

    Initial commit

---
+++ hw1/task4/var4/index.php
@@ -0,0 +1,12 @@
+﻿<?php
+	$title = "Главная страница - страница обо мне";
+	$h1 = "Информация обо мне";
+	$curYear = "2018";
+
+	$content = file_get_contents("site.html");
+
+	$content = str_replace("{{ title }}", $title, $content);
+	$content = str_replace("{{ h1 }}", $h1, $content);
+	$content = str_replace("{{ curYear }}", $curYear, $content);
+
+	echo $content;
